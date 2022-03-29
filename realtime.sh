428361033d320a08f61d209b6e68ed52411660fc
radhikamada <100508512+radhikamada@users.noreply.github.com>
2022-03-29 12:57:16 +0530

    realtime
    gg

---
+++ realtime.sh
@@ -0,0 +1,14 @@
+  agent any
+
+    stages {
+        stage('Build') {
+            steps {
+                // Get some code from a GitHub repository
+                git 'https://github.com/RAKHEE001/JAVA-APP.git'
+
+                // Run Maven on a Unix agent.
+                sh "mvn -Dmaven.test.failure.ignore=true clean package"
+
+                // To run Maven on a Windows agent, use
+                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
+            }
